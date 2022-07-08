5b8798602d08678fbcaa0727de1755598a91cee3
rsalesfreitas <rsales.freitas@live.com>
2022-07-08 11:52:42 -0300

    alterando modulo de ec2 para chamada

--- main.tf
+++ main.tf
@@ -3,13 +3,3 @@ provider "aws" {
   version = ">= 1.2.0"
   profile = "dev"
 }
-
-terraform {
-  backend "s3" {
-    # Lembre de trocar o bucket para o seu
-    bucket = "s3-tfstates-desenvolvimento"
-    key    = "desenvolvimento"
-    region = "us-west-2"
-    profile = "dev"
-  }
-}
